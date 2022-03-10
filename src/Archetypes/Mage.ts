f746f529d656aab595fa601a85cff71308920112
Fernando Serpa <serpaef@outlook.com>
2022-03-10 18:17:15 -0300

    feat: Cria classe Mage

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,19 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private _energyType: EnergyType = 'mana';
+
+  constructor(name: string) {
+    super(name);
+    Mage.instances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Mage.instances;
+  }
+}
