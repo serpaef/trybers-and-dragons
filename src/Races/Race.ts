6da20e2d0b4027180b1d2d021bfbdc1991ac3c78
Fernando Serpa <serpaef@outlook.com>
2022-03-10 16:41:41 -0300

    fix: minor fixes: set vida maxima e...
    implementação método createdRacesInstances

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,15 +1,17 @@
 export default abstract class Race {
   readonly name: string;
   readonly dexterity: number;
+  static instances = 0;
   
   constructor(name: string, dexterity: number) {
     this.name = name;
     this.dexterity = dexterity;
   }
   
+  abstract set maxLifePoints(lifePoints: number);
   abstract get maxLifePoints(): number;
 
-  static createdRacesInstances = ():number => {
+  static createdRacesInstances() {
     throw new Error('Not implemented');
-  };
+  }
 }
