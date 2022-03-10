4af1c669564d2ec6414c83e03a3cbbbc2d6825e0
Fernando Serpa <serpaef@outlook.com>
2022-03-10 18:23:40 -0300

    feat: Cria classe Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,19 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType = 'stamina';
+
+  constructor(name: string) {
+    super(name);
+    Warrior.instances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Warrior.instances;
+  }
+}
